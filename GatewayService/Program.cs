7523ea7a0d9bcd8f64eb742adff69680d37fc4c2
Alejandro Montaner Roig <alroigcode@gmail.com>
2025-02-24 16:30:53 +0100

    Ocelot Rate Limiting and Jwt Security on Gateway level.

--- GatewayService/Program.cs
+++ GatewayService/Program.cs
@@ -1,10 +1,43 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+var jwtSettings = builder.Configuration.GetSection("Jwt");
+var keyString = jwtSettings["Key"];
+if (string.IsNullOrEmpty(keyString))
+{
+	throw new InvalidOperationException("Jwt key is missing");
+}
+var key = Encoding.UTF8.GetBytes(keyString);
+
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+	.AddJwtBearer(options => 
+	{ 
+		options.TokenValidationParameters = new TokenValidationParameters()
+		{
+			ValidateIssuer = true,
+			ValidateAudience = true,
+			ValidateLifetime = true,
+			ValidateIssuerSigningKey = true,
+			ValidIssuer = jwtSettings["issuer"],
+			ValidAudience = jwtSettings["audience"],
+			IssuerSigningKey = new SymmetricSecurityKey(key)
+		};
+	});
+
+
+builder.Services.AddOcelot(builder.Configuration);
+builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
 
 var app = builder.Build();
 
@@ -16,8 +49,24 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
+// Custom middleware to set rate limit client ID from JWT
+app.Use(async (context, next) =>
+{
+	if (!context.User.Identity.IsAuthenticated)
+	{
+		var sub = context.User.FindFirst("sub")?.Value;
+		if (!string.IsNullOrEmpty(sub))
+		{
+			context.Request.Headers["Client-Id"] = sub; // Use JWT sub as client ID
+		}
+	}
+	await next.Invoke();
+});
+
+app.UseOcelot().Wait();
 app.MapControllers();
 
 app.Run();
